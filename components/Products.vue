48ceec0a771ff71161fe8469cafa3c0f7aadea2f
trenntoilette-org <trenntoilette.org@protonmail.com>
2022-05-09 18:06:08 +0200

    dev

--- components/Products.vue
+++ components/Products.vue
@@ -3,7 +3,7 @@
     <div
       v-for="(product, index) in products"
       :key="index"
-      class="col-lg-2 fadeInUp"
+      class="col-lg-2 col-md-4 col-sm-6 col-xs-12 fadeInUp"
     >
       <ProductItem :product="product" class="mt-5" />
     </div>
